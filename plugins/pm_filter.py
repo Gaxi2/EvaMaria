915407dc36336b49569c318d300348de3b6bc27f
Gaxi2 <103119685+Gaxi2@users.noreply.github.com>
2022-06-13 14:40:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -136,7 +136,8 @@ async def advantage_spoll_choker(bot, query):
             await auto_filter(bot, query, k)
         else:
             k = await query.message.edit("""This Movie Not Found In DataBase.
-                                         <b>Please make a request in this format: #request Movie_Name, Movie_Year, Movie_Quality and #hindi (for dub)</b>""")
+             
+            <b>Please make a request in this format: #request Movie_Name, Movie_Year, Movie_Quality and #hindi (for dub)</b>""")
             await asyncio.sleep(10)
             await k.delete()
 
