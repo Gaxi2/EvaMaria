fc699c511e69642e0ce015ad27089563e57d3897
Gaxi2 <103119685+Gaxi2@users.noreply.github.com>
2022-07-22 08:43:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -721,11 +721,12 @@ async def auto_filter(client, msg, spoll=False):
         try:
          a = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
                                       reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(30)
+         await asyncio.sleep(30)
+         await message.delete()
+         await a.delete()
+            
 
-            await message.delete()
 
-            await a.delete()
             
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
